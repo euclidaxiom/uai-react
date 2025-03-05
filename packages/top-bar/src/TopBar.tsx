fb24a6ccdecca06b400d6010653a043f8f8085cd
euclidaxiom <email.grco@gmail.com>
2025-03-05 17:24:12 -0300

    Update TopBar

--- packages/top-bar/src/TopBar.tsx
+++ packages/top-bar/src/TopBar.tsx
@@ -19,38 +19,31 @@ interface TopBar extends React.HTMLAttributes<HTMLDivElement> {
 
 const TopBar = forwardRef<HTMLDivElement, TopBar>(({ children }, ref) => {
   const showTitle = true;
-  const title = "Title";
 
   return (
     <div ref={ref} className="uai-Top-bar">
       <div className="leading">
-        {/* <Button size="32">
+        <Button size="32">
           <ButtonIcon>
             <span className="material-symbols-sharp">dock_to_right</span>
           </ButtonIcon>
-        </Button> */}
+        </Button>
 
         {/* <div>
-            <Button size="32">
-              <ButtonIcon>
-                <span className="material-symbols-sharp">
-                  arrow_back_ios_new
-                </span>
-              </ButtonIcon>
-            </Button>
-            <Button size="32">
-              <ButtonIcon>
-                <span className="material-symbols-sharp">
-                  arrow_forward_ios
-                </span>
-              </ButtonIcon>
-            </Button>
-          </div> */}
-
-        {showTitle && <div>{title}</div>}
+          <Button size="32">
+            <ButtonIcon>
+              <span className="material-symbols-sharp">arrow_back_ios_new</span>
+            </ButtonIcon>
+          </Button>
+          <Button size="32">
+            <ButtonIcon>
+              <span className="material-symbols-sharp">arrow_forward_ios</span>
+            </ButtonIcon>
+          </Button>
+        </div> */}
       </div>
-      <div className="center">{children}</div>
-      <div className="trailing">{children}</div>
+      <div className="center">{showTitle && <div>{children}</div>}</div>
+      <div className="trailing"></div>
     </div>
   );
 });
